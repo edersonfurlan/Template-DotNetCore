d326ad334adc5e4d6220af636a9c0378eece00fc
edersonfurlan <edersonmello@msn.com>
2021-12-27 09:04:45 -0300

    Finalizado Trabalhando com Senhas (Password) em uma aplicação .Net Core com Angular

--- Template/ClientApp/src/app/app.module.ts
+++ Template/ClientApp/src/app/app.module.ts
@@ -7,16 +7,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { UsersComponent } from './users/users.component';
+import { UserDataService } from './_data-services/user.data-service';
+import { Interceptor } from './app.interceptor.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    UsersComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,11 +24,11 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: 'users', component: UsersComponent },
+    ]),
+    Interceptor
   ],
-  providers: [],
+  providers: [UserDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
